66246b303ae4d89c975fa1c98e218eddea4aa972
Daniel Perez <edanielprz@gmail.com>
2026-02-16 21:25:13 -0600

    cambios en config, con usuario de github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://edanielprz.github.io',
+  base: '/portfolio/',
   build: {
     format: 'file'
   }
